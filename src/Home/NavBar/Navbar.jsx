9fd76e76b52b9341661019c01cee9cb32309aea3
podrozujem <admin@admins-MacBook-Pro-3.local>
2023-08-01 10:05:42 +0200

    Eliminated bootstrap from project

--- src/Home/NavBar/Navbar.jsx
+++ src/Home/NavBar/Navbar.jsx
@@ -31,44 +31,88 @@ function Navbar() {
   });
 
   return (
-    <NavbarBs className='navigation'>
-      <Container>
-        <Nav>
+    // <NavbarBs className='navigation'>
+    //   <Container>
+    //     <Nav>
+    //       <NavLink to='/' as={NavLink} className='navigation__button navigation__button--logo'>
+    //         <img src={logo} alt='logo'></img>
+    //       </NavLink>
+
+    //       <NavLink to='/cart' as={NavLink} className='navigation__button'>
+    //         Shop
+    //       </NavLink>
+
+    //       <Button
+    //         className='navigation__cart-icon'
+    //         onClick={() => {
+    //           setOpenCart(!openCart);
+    //         }}
+    //       >
+    //         <svg
+    //           className='w-10 h-10'
+    //           fill='none'
+    //           stroke='currentColor'
+    //           viewBox='0 0 24 24'
+    //           xmlns='http://www.w3.org/2000/svg'
+    //         >
+    //           <path
+    //             strokeLinecap='round'
+    //             strokeLinejoin='round'
+    //             strokeWidth={2}
+    //             d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+    //           />
+    //         </svg>
+    //         <div className='navigation__cart-number'> {cartItems.length}</div>
+    //       </Button>
+    //     </Nav>
+
+    //     {openCart && <Dropdown openCart={openCart} cartItems={cartItems} />}
+    //   </Container>
+    // </NavbarBs>
+
+    <nav className='navigation'>
+      <div className='container'>
+        <div className='navigation__button navigation__button--logo'>
           <NavLink to='/' as={NavLink} className='navigation__button navigation__button--logo'>
             <img src={logo} alt='logo'></img>
           </NavLink>
-
-          <NavLink to='/cart' as={NavLink} className='navigation__button'>
-            Shop
-          </NavLink>
-
-          <Button
-            className='navigation__cart-icon'
-            onClick={() => {
-              setOpenCart(!openCart);
-            }}
-          >
-            <svg
-              className='w-10 h-10'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-              />
-            </svg>
-            <div className='navigation__cart-number'> {cartItems.length}</div>
-          </Button>
-        </Nav>
-
+        </div>
+        <div className='nav-elements'>
+          <ul>
+            <li>
+              <NavLink to='/cart' className='navigation__button'>
+                Shop
+              </NavLink>
+            </li>
+            <li>
+              <Button
+                className='navigation__cart-icon'
+                onClick={() => {
+                  setOpenCart(!openCart);
+                }}
+              >
+                <svg
+                  className='w-10 h-10'
+                  fill='none'
+                  stroke='currentColor'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                  />
+                </svg>
+                <div className='navigation__cart-number'> {cartItems.length}</div>
+              </Button>
+            </li>
+          </ul>
+        </div>
         {openCart && <Dropdown openCart={openCart} cartItems={cartItems} />}
-      </Container>
-    </NavbarBs>
+      </div>
+    </nav>
   );
 }
 export default Navbar;
